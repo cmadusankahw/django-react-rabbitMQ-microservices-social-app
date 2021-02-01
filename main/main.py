65d36df33f9a757c4bed823c276f36e85ff86665
Chiran Hewawitharana <chiranh@zone24x7.com>
2021-02-02 00:29:23 +0530

    added SQLAlchemy implementation for flask app

--- main/main.py
+++ main/main.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+CORS(app)  # enabling CORS policies for front-end app
+
+db = SQLAlchemy(app)  # creating mysql database
+
+
+# models
+class Product(db.Model):  # Product imported from django app through RabbitMQ
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
 @app.route('/')
